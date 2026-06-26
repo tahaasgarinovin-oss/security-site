e70cb74778245073c4dc7cddd8c254dc70d40243
tahaasgarinovin <tahaasgarinovin@gmail.com>
2026-06-26 09:36:31 -0700

    fix deploy settings

--- settings.py
+++ settings.py
@@ -2,16 +2,18 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-change-this'
 
-# production
+SECRET_KEY = "django-insecure-temp-key-change-this"
+
+# Render = production
 DEBUG = False
+
 
 ALLOWED_HOSTS = [
     "security-site.onrender.com",
-    ".onrender.com",
-    "127.0.0.1",
+    "*.onrender.com",
     "localhost",
+    "127.0.0.1",
 ]
 
 INSTALLED_APPS = [
@@ -21,6 +23,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'recruitment',
 ]
 
@@ -73,5 +76,7 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
